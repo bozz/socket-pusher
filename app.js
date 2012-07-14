12f703e7eaabb960698aa59ec76033276670062b
remared <network@amer8.de>
2012-07-14 11:08:46 +0200

    added static file server, basic working socket.io

---
+++ app.js
@@ -0,0 +1,59 @@
+var libpath = require('path'),
+    http = require("http").createServer(handler),
+    io = require('socket.io').listen(http),
+    fs = require('fs'),
+    url = require("url"),
+    mime = require('mime');
+
+var path = ".";
+var port = 8080;
+
+
+http.listen(port);
+
+function handler(request, response) {
+    var uri = url.parse(request.url).pathname;
+    var filename = libpath.join(path, uri);
+    console.log('filename', filename);
+
+    fs.exists(filename, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) {
+            filename += '/index.html';
+        }
+
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                response.writeHead(500, {
+                    "Content-Type": "text/plain"
+                });
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+
+            var type = mime.lookup(filename);
+            response.writeHead(200, {
+                "Content-Type": type
+            });
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}
+
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});

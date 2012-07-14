44a828fb1e861b577d674eadcdad4276588453da
remared <network@amer8.de>
2012-07-14 16:25:15 +0200

    added highstock.js library, defined chart in index.html

--- js/main.js
+++ js/main.js
@@ -4,3 +4,71 @@ socket.on('time', function (data) {
   // console.log(data.berlin);
   // socket.emit('my other event', { my: 'data' });
 });
+
+
+
+$(function() {
+    
+    Highcharts.setOptions({
+		global : {
+			useUTC : false
+		}
+	});
+
+		// create the chart
+		var chart = new Highcharts.StockChart({
+			chart : {
+				renderTo : 'chart'
+			},
+
+			title: {
+				text: 'AAPL stock price by minute'
+			},
+			
+			rangeSelector : {
+				buttons : [{
+					type : 'hour',
+					count : 1,
+					text : '1h'
+				}, {
+					type : 'day',
+					count : 1,
+					text : '1D'
+				}, {
+					type : 'all',
+					count : 1,
+					text : 'All'
+				}],
+				selected : 1,
+				inputEnabled : false
+			},
+            
+            exporting: {
+                enabled: false
+            },
+			
+			series : [{
+				name : 'AAPL',
+				type: 'candlestick',
+				data : (function() {
+                    // generate an array of random data
+                    var data = [], time = (new Date()).getTime(), i;
+
+                    for( i = -999; i <= 0; i++) {
+                        data.push([
+                            time + i * 1000,
+                            Math.round(Math.random() * 100),
+                            Math.round(Math.random() * 100),
+                            Math.round(Math.random() * 100),
+                            Math.round(Math.random() * 100)
+                        ]);
+                    }
+                    return data;
+                })(),
+				tooltip: {
+					valueDecimals: 2
+				}
+			}]
+		});
+        
+});
